46562a044ba4e021afa06f01168f6f312c0f7dc9
HM <hieumdd@gmail.com>
2023-08-14 16:23:27 +0700

    feat: Add cloud-tasks

--- src/pipeline/pipeline.service.ts
+++ src/pipeline/pipeline.service.ts
@@ -5,8 +5,9 @@ import ndjson from 'ndjson';
 
 import { logger } from '../logging.service';
 import { createLoadStream } from '../bigquery.service';
+import { createTasks } from '../cloud-tasks.service';
 import { GetResourcesOptions, getResources } from '../outreach/resource.service';
-import { Pipeline } from './pipeline.const';
+import * as pipelines from './pipeline.const';
 
 const transformValidation = (schema: Joi.Schema) => {
     return new Transform({
@@ -27,7 +28,7 @@ const transformValidation = (schema: Joi.Schema) => {
 
 export type RunPipelineOptions = GetResourcesOptions;
 
-export const runPipeline = async (pipeline_: Pipeline, options: RunPipelineOptions) => {
+export const runPipeline = async (pipeline_: pipelines.Pipeline, options: RunPipelineOptions) => {
     logger.info({ action: 'start', pipeline: pipeline_.loadConfig.table });
 
     const stream = await getResources(pipeline_.getConfig, options);
@@ -41,4 +42,13 @@ export const runPipeline = async (pipeline_: Pipeline, options: RunPipelineOptio
             schema: pipeline_.loadConfig.schema,
         }),
     ).then(() => ({ table: pipeline_.loadConfig.table }));
+};
+
+export type CreatePipelineTasksOptions = GetResourcesOptions;
+
+export const createPipelineTasks = async ({ start, end }: CreatePipelineTasksOptions) => {
+    return createTasks(
+        Object.keys(pipelines).map((pipeline_) => ({ pipeline: pipeline_, start, end })),
+        (task) => task.pipeline,
+    );
 };
