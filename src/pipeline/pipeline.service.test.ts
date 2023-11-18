57ec294b4f6ff1f35159bd0fa9a5e5ab20d0752d
HM <hieumdd@gmail.com>
2023-11-18 13:30:21 +0700

    misc: Add run-multiple-pipeline case

--- src/pipeline/pipeline.service.test.ts
+++ src/pipeline/pipeline.service.test.ts
@@ -1,3 +1,4 @@
+import * as pipelines from './pipeline.const';
 import { createPipelineTasks, runPipeline } from './pipeline.service';
 
 it('run-pipeline', async () => {
@@ -17,6 +18,26 @@ it('run-pipeline', async () => {
         });
 }, 100_000_000);
 
+describe('run-multiple-pipeline', () => {
+    it.each(Object.keys(pipelines).map((_, i) => i))('$x', async (index) => {
+        process.env['CLOUD_RUN_TASK_INDEX'] = `${index}`;
+
+        return runPipeline({
+            userId: '16d06ab5-ae10-451d-af33-53f29ff93a12',
+            options: {
+                start: '2022-01-01',
+                end: '2023-01-01',
+            },
+        })
+            .then((result) => {
+                console.log({ result });
+            })
+            .catch((error) => {
+                console.error({ error });
+                throw error;
+            });
+    });
+});
 it('create-pipeline-tasks', async () => {
     return createPipelineTasks({ start: '', end: '' })
         .then((result) => expect(result).toBeDefined())
