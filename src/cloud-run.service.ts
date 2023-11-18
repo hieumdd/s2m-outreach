fbe11127e82e9051b2a8e8312dfc3fd8a7fd7ff5
HM <hieumdd@gmail.com>
2023-11-18 13:59:37 +0700

    fix: Fix job path

--- src/cloud-run.service.ts
+++ src/cloud-run.service.ts
@@ -3,8 +3,10 @@ import { JobsClient } from '@google-cloud/run';
 const client = new JobsClient();
 
 export const executeJob = async (args: string[], taskCount: number) => {
-    return client.runJob({
-        name: `s2m-outreach-executor`,
+    const projectId = await client.getProjectId();
+
+    return await client.runJob({
+        name: client.jobPath(projectId, 'us-central1', 's2m-outreach-executor'),
         overrides: { containerOverrides: [{ args }], taskCount },
     });
 };
